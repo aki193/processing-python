30e21a6763c8681e3469bff708fcb76305cf0d01
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-10-30 22:06:51 +0900

    画像を２値化して輪郭抽出を行なった

--- opencv-python/test01.py
+++ opencv-python/test01.py
@@ -2,5 +2,11 @@
 import cv2
 
 im = cv2.imread("pic01.jpg")
-cv2.imshow('image', im)
+
+
+imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(imgray,127,255,0)
+image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.imshow('image', image)
 cv2.waitKey(0)
