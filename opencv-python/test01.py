18cb8bc8879ce8b5d1a7af73f17a0cdfe96068e1
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-10-30 23:20:40 +0900

    リファクタリングしてコメントを付与した

--- opencv-python/test01.py
+++ opencv-python/test01.py
@@ -2,31 +2,33 @@
 import cv2
 import math
 
+# 円形度計算
 def calcCircleLevel (contour, area):
     perimeter = cv2.arcLength(contour, True)
     circle_level = 4.0 * math.pi * area / (perimeter * perimeter); # perimeter = 0 のとき気をつける
     return circle_level
 
-im = cv2.imread("data/pic45.jpg")
+# 画像読み込み
+imgOrigin = cv2.imread("data/pic45.jpg")
+# グレースケールへ変換後に２値化する
+imgGray = cv2.cvtColor(imgOrigin,cv2.COLOR_BGR2GRAY)
+ret,imgThresh = cv2.threshold(imgGray,220,255,cv2.THRESH_BINARY)
+# ２値化した画像から輪郭を取得
+imgTmp, contours, hierarchy = cv2.findContours(imgThresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 
-
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-#ret,thresh = cv2.threshold(imgray,20,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-ret,thresh = cv2.threshold(imgray,220,255,cv2.THRESH_BINARY)
-#image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+# 取得した輪郭に対して処理を行う（輪郭は複数取れている）
 for contour in contours:
-    img = cv2.drawContours(im, contour, -1, (0, 255, 0), 3)
+    # 輪郭を（緑色で太さ３で <- 引数の値）描画する
+    imgContour = cv2.drawContours(imgOrigin, contour, -1, (0, 255, 0), 3)
+    # 輪郭の面積を算出
     area = cv2.contourArea(contour)
     print(area)
+    # 輪郭があったら円形度を算出する（面積がある場合に処理をする．輪郭があっても面積が０の時があるため．）
     if area != 0:
         level = calcCircleLevel(contour, area)
         print(level)
-#img = cv2.drawContours(im, contours[], -1, (0, 255, 0), 3)
 
-#img = cv2.drawContours(image, contours[0], -1, (0,255,0), 3)
-
-cv2.imshow('image', img)
-cv2.imshow('image2', thresh)
+cv2.imshow('image', imgContour)
+cv2.imshow('image2', imgThresh)
 cv2.waitKey(0)
 
