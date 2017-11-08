d36fea8f18cde7d446191e32e0b1177521b21da0
Unknown <b1500512@planet.kanazawa-it.ac.jp>
2017-11-09 00:13:31 +0900

    配列を用いて、60枚の画像の円形度を求めようとした。

--- opencv-python/circularity.py
+++ opencv-python/circularity.py
@@ -1,6 +1,8 @@
-
 import cv2
 import math
+
+img0rigin = [61]
+#img0rigin_sum = [60]
 
 # 円形度計算
 def calcCircleLevel (contour, area):
@@ -10,30 +12,35 @@ def calcCircleLevel (contour, area):
 
 # 60枚の画像読み込み
 for i in range(1, 10):
-	cv2.imread("data/pic0"+str(i)+".jpg")
-# 画像を読み込んだ後に60枚の円形度を算出します。
-	
-	
+	img0rigin[i] = cv2.imread("data/pic0"+str(i)+".jpg")
 
+for i in range(10, 61):
+	img0rigin[i] = cv2.imread("data/pic"+str(i)+".jpg")
+    
+#for i in range(61):
+#	img0rigin_sum[i].append(img0rigin[i])
+	
 # 画像読み込み
-imgOrigin = cv2.imread("data/pic45.jpg")
-# グレースケールへ変換後に２値化する
-imgGray = cv2.cvtColor(imgOrigin,cv2.COLOR_BGR2GRAY)
-ret,imgThresh = cv2.threshold(imgGray,220,255,cv2.THRESH_BINARY)
-# ２値化した画像から輪郭を取得
-imgTmp, contours, hierarchy = cv2.findContours(imgThresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+#imgOrigin = cv2.imread("data/pic45.jpg")
+
+for i in range(1, 61):
+    # グレースケールへ変換後に２値化する
+    imgGray = cv2.cvtColor(imgOrigin[i],cv2.COLOR_BGR2GRAY)
+    ret,imgThresh = cv2.threshold(imgGray,220,255,cv2.THRESH_BINARY)
+    # ２値化した画像から輪郭を取得
+    imgTmp, contours, hierarchy = cv2.findContours(imgThresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 
 # 取得した輪郭に対して処理を行う（輪郭は複数取れている）
-for contour in contours:
-    # 輪郭を（緑色で太さ３で <- 引数の値）描画する
-    imgContour = cv2.drawContours(imgOrigin, contour, -1, (0, 255, 0), 3)
-    # 輪郭の面積を算出
-    area = cv2.contourArea(contour)
-    print(area)
-    # 輪郭があったら円形度を算出する（面積がある場合に処理をする．輪郭があっても面積が０の時があるため．）
-    if area != 0:
-        level = calcCircleLevel(contour, area)
-        print(level)
+    for contour in contours:
+        # 輪郭を（緑色で太さ３で <- 引数の値）描画する
+        imgContour = cv2.drawContours(imgOrigin[i], contour, -1, (0, 255, 0), 3)
+        # 輪郭の面積を算出
+        area = cv2.contourArea(contour)
+        print(area)
+        # 輪郭があったら円形度を算出する（面積がある場合に処理をする．輪郭があっても面積が０の時があるため．）
+        if area != 0:
+            level = calcCircleLevel(contour, area)
+            print(level)
 # 画像の表示
 cv2.imshow('image contour', imgContour)
 cv2.imshow('image threshold', imgThresh)
