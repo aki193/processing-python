91b4109640521b5769c2ac033cc3f84d2932d242
Unknown <b1500512@planet.kanazawa-it.ac.jp>
2017-11-09 14:29:27 +0900

    60枚の画像の円形度を算出２

--- opencv-python/circularity.py
+++ opencv-python/circularity.py
@@ -1,8 +1,8 @@
 import cv2
 import math
 
-img0rigin = [61]
-#img0rigin_sum = [60]
+img0rigin = []
+img0rigin_sum = []
 
 # 円形度計算
 def calcCircleLevel (contour, area):
@@ -12,20 +12,21 @@ def calcCircleLevel (contour, area):
 
 # 60枚の画像読み込み
 for i in range(1, 10):
-	img0rigin[i] = cv2.imread("data/pic0"+str(i)+".jpg")
+	img0rigin = cv2.imread("data/pic0"+str(i)+".jpg")
 
 for i in range(10, 61):
-	img0rigin[i] = cv2.imread("data/pic"+str(i)+".jpg")
+	img0rigin = cv2.imread("data/pic"+str(i)+".jpg")
     
-#for i in range(61):
-#	img0rigin_sum[i].append(img0rigin[i])
+for i in range(60):
+	img0rigin_sum.append(img0rigin[i])
 	
 # 画像読み込み
 #imgOrigin = cv2.imread("data/pic45.jpg")
 
-for i in range(1, 61):
+#for i in range(60):
+
     # グレースケールへ変換後に２値化する
-    imgGray = cv2.cvtColor(imgOrigin[i],cv2.COLOR_BGR2GRAY)
+    imgGray = cv2.cvtColor(img0rigin_sum[i],cv2.COLOR_BGR2GRAY)
     ret,imgThresh = cv2.threshold(imgGray,220,255,cv2.THRESH_BINARY)
     # ２値化した画像から輪郭を取得
     imgTmp, contours, hierarchy = cv2.findContours(imgThresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
@@ -33,7 +34,7 @@ for i in range(1, 61):
 # 取得した輪郭に対して処理を行う（輪郭は複数取れている）
     for contour in contours:
         # 輪郭を（緑色で太さ３で <- 引数の値）描画する
-        imgContour = cv2.drawContours(imgOrigin[i], contour, -1, (0, 255, 0), 3)
+        imgContour = cv2.drawContours(img0rigin_sum[i], contour, -1, (0, 255, 0), 3)
         # 輪郭の面積を算出
         area = cv2.contourArea(contour)
         print(area)
