9d50f9c7aaadf53d6b7ef03669a1fc2867b3f9ea
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-11-10 17:30:37 +0900

    画像の出力処理を切り分けた

--- opencv-python/circularity.py
+++ opencv-python/circularity.py
@@ -34,15 +34,16 @@ for i in range(10, 61):
 # 画像の読み込み
 for i in range(60):
     images.append(cv2.imread(imgPath[i]))
-    # cv2.imshow("image" + str(i), images[i])
 
 # グレースケールへ変換後に２値化する
 for i in range(60):
     imgGray = cv2.cvtColor(images[i],cv2.COLOR_BGR2GRAY)
     ret,imgThresh = cv2.threshold(imgGray,220,255,cv2.THRESH_BINARY)
+
 # ２値化した画像から輪郭を取得
     imgTmp, contours, hierarchy = cv2.findContours(imgThresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     imgContours.append(contours)
+
 for i in range(60):
     for contour in imgContours[i]:
     # 輪郭の面積を算出
@@ -53,38 +54,11 @@ for i in range(60):
             imgContour.append(cv2.drawContours(images[i], contour, -1, (0, 255, 0), 3))
             # 中央にテキスト描く
             cv2.putText(images[i],str(level),(25,40),fontface,fontscale, color)
-            cv2.imshow("image" + str(i+1), images[i])
             print("No:" + str(i+1), OKBLUE + "Area = " + str(area), OKGREEN + "Level = " + str(level))
-            break
+            break   # １度輪郭を取得したら次の画像を解析する
 
-# for i in range(60):
-#     for contour in imgContours[i]:
-#         imgContour.append(cv2.drawContours(images[i], contour, -1, (0, 255, 0), 3))
-
-# # 取得した輪郭に対して処理を行う（輪郭は複数取れている）
-# for i in range(60):
-#     for contour in contours:
-#     # 輪郭を（緑色で太さ３で <- 引数の値）描画する
-#         imgContour = cv2.drawContours(images[i], contour, -1, (0, 255, 0), 3)
-#     # 輪郭の面積を算出
-#         area = cv2.contourArea(contour)
-#         print(area)
-#      # 輪郭があったら円形度を算出する（面積がある場合に処理をする．輪郭があっても面積が０の時があるため．）
-#         if area != 0:
-#             level = calcCircleLevel(contour, area)
-#             print(level)
-
-# cv2.imshow("image contour"+str(0), imgContour[0])
-
-# 画像の表示
-# for i in range(60):
-#     cv2.imshow("image contour"+str(i), imgContour[i])
-#     cv2.imshow("image threshold"+str(i), imgThresh[i])
-# for i in range(10):
-#     cv2.imshow("image" + str(i), images[i])
-
-# cv2.imshow("image", images[0])
-# cv2.imshow("original image", images[3])
+for i in range(60):
+    cv2.imshow("image" + str(i+1), images[i])
 
 # キーが入力されるまで終了しない
 cv2.waitKey(0)
