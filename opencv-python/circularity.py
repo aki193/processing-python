b1e60a0d85695734cadcf24886fd0f94593164e8
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-11-10 16:15:06 +0900

    ６０枚の画像を読み込み，１枚の画像の輪郭を出力した

--- opencv-python/circularity.py
+++ opencv-python/circularity.py
@@ -4,7 +4,8 @@ import math
 
 imgPath = []
 images = []
-
+imgContours = []
+imgContour = []
 # 円形度計算
 def calcCircleLevel (contour, area):
     perimeter = cv2.arcLength(contour, True)
@@ -28,24 +29,32 @@ for i in range(60):
     ret,imgThresh = cv2.threshold(imgGray,220,255,cv2.THRESH_BINARY)
 # ２値化した画像から輪郭を取得
     imgTmp, contours, hierarchy = cv2.findContours(imgThresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    imgContours.append(contours)
 
-# 取得した輪郭に対して処理を行う（輪郭は複数取れている）
-for i in range(60):
-    for contour in contours:
-    # 輪郭を（緑色で太さ３で <- 引数の値）描画する
-        imgContour = cv2.drawContours(images[i], contour, -1, (0, 255, 0), 3)
-    # 輪郭の面積を算出
-        area = cv2.contourArea(contour)
-        print(area)
-     # 輪郭があったら円形度を算出する（面積がある場合に処理をする．輪郭があっても面積が０の時があるため．）
-        if area != 0:
-            level = calcCircleLevel(contour, area)
-            print(level)
-            
+print(imgContours[0])
+
+for contour in imgContours[0]:
+    imgContour.append(cv2.drawContours(images[0], contour, -1, (0, 255, 0), 3))
+
+# # 取得した輪郭に対して処理を行う（輪郭は複数取れている）
+# for i in range(60):
+#     for contour in contours:
+#     # 輪郭を（緑色で太さ３で <- 引数の値）描画する
+#         imgContour = cv2.drawContours(images[i], contour, -1, (0, 255, 0), 3)
+#     # 輪郭の面積を算出
+#         area = cv2.contourArea(contour)
+#         print(area)
+#      # 輪郭があったら円形度を算出する（面積がある場合に処理をする．輪郭があっても面積が０の時があるため．）
+#         if area != 0:
+#             level = calcCircleLevel(contour, area)
+#             print(level)
+
+# cv2.imshow("image contour"+str(0), imgContour[0])
+
 # 画像の表示
-for i in range(60):
-    cv2.imshow("image contour"+str(i), imgContour[i])
-    cv2.imshow("image threshold"+str(i), imgThresh[i])
-
+# for i in range(60):
+#     cv2.imshow("image contour"+str(i), imgContour[i])
+#     cv2.imshow("image threshold"+str(i), imgThresh[i])
+cv2.imshow("image", imgContour[0])
 # キーが入力されるまで終了しない
 cv2.waitKey(0)
