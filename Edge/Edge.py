c27fd2512e972cb1d6aa2a31e1f56fd4496aea0c
Unknown <b1504479@planet.kanazawa-it.ac.jp>
2017-12-31 22:39:05 +0900

    Extraction for corner

--- Edge/Edge.py
+++ Edge/Edge.py
@@ -8,35 +8,19 @@ from matplotlib import pyplot as plt
 img = cv2.imread("data/pic34.jpg",0)
 
 #画像をエッジを抽出
-imgCanny = cv2.Canny(img,200,255)
+imgCanny = cv2.Canny(img,500,550)
 
-hist = cv2.calcHist([imgCanny],[0],None,[256],[0,256])
+corners = cv2.goodFeaturesToTrack(imgCanny,100,0.1,15)
+corners = np.int0(corners)
 
-print(hist)
+cornersPoint = 0
+for i in corners:
+    x,y = i.ravel()
+    cv2.circle(imgCanny,(x,y),3, 100, -1)
+    cornersPoint += 1
 
-plt.xlim(0,255)
-plt.plot(hist)
-plt.xlabel("Pixel value", fontsize = 20)
-plt.ylabel("Number of pixels", fontsize = 20)
-plt.grid()
-plt.show()
+print(cornersPoint)
+plt.imshow(imgCanny),plt.show()
 
-
-
-cv2.namedWindow("imge", cv2.WINDOW_NORMAL)
-cv2.imshow("imge",imgCanny)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-#
-# # 画像パスの生成（pic1~9）
-# for i in range(1,10):
-#     imgPath.append("data/pic0"+str(i)+".jpg")
-#
-# # 画像パスの生成(pic10~60)
-# for i in range(10, 61):
-#     imgPath.append("data/pic"+str(i)+".jpg")
-#
-# # 画像の読み込み（pic1~60）
-# for i in range(60):
-#     images.append(cv2.imread(imgPath[i]))
