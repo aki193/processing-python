fd1d32eea09dee2aac918fb8c01ac98c74944b1d
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2018-01-04 21:57:25 +0900

    Added plot animation

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -20,24 +20,12 @@ def train(wvec,xvec,label):
     else:
         return wvec
 
-def plot(i, wvec):
-    for w in wvec:
-        plt.cla()
-        x_fig=range(-15,16)
-        y_fig=[-(w[1]/w[0])*xi-(w[2]/w[1]) for xi in x_fig]
-
-        plt.scatter(x1[:,0],x1[:,1],marker='o',color='g',s=100)
-        plt.scatter(x2[:,0],x2[:,1],marker='s',color='b',s=100)
-        im = plt.plot(x_fig,y_fig)
-
 if __name__=='__main__':
 
     item_num=100
     loop=1000
     init_wvec=[1,-1,1]
     isUpdate = True     # 収束判定
-    ims = []
-
 
     x1_1=np.ones(int(item_num/2))+10*np.random.random(int(item_num/2))
     x1_2=np.ones(int(item_num/2))+10*np.random.random(int(item_num/2))
@@ -65,29 +53,18 @@ if __name__=='__main__':
                 isUpdate = True
             wvec=np.vstack((wvec,wvec_new))
 
-    # w=wvec[-1]
-    # print w
-
+    # アニメーション
     fig = plt.figure()
+    ims = []
 
-    # for w in wvec:
-    #     x_fig=range(-15,16)
-    #     y_fig=[-(w[1]/w[0])*xi-(w[2]/w[1]) for xi in x_fig]
-    #
-    #     plt.scatter(x1[:,0],x1[:,1],marker='o',color='g',s=100)
-    #     plt.scatter(x2[:,0],x2[:,1],marker='s',color='b',s=100)
-    #     im = plt.plot(x_fig,y_fig)
-    #     ims.append(im)
+    for w in wvec:
+        x_fig=range(-15,16)
+        y_fig=[-(w[1]/w[0])*xi-(w[2]/w[1]) for xi in x_fig]
 
-    # ani = animation.ArtistAnimation(fig, ims, interval=100)
-    ani = animation.FuncAnimation(fig, plot, fargs=wvec, interval=100, frames=10)
+        plt.scatter(x1[:,0],x1[:,1],marker='o',color='g',s=100)
+        plt.scatter(x2[:,0],x2[:,1],marker='s',color='b',s=100)
+        im = plt.plot(x_fig,y_fig)
+        ims.append(im)
+
+    ani = animation.ArtistAnimation(fig, ims, interval=100)
     plt.show()
-
-    # ims.append(im)
-    # x_fig=range(-15,16)
-    # y_fig=[-(w[1]/w[0])*xi-(w[2]/w[1]) for xi in x_fig]
-    #
-    # plt.scatter(x1[:,0],x1[:,1],marker='o',color='g',s=100)
-    # plt.scatter(x2[:,0],x2[:,1],marker='s',color='b',s=100)
-    # plt.plot(x_fig,y_fig)
-    # plt.show()
