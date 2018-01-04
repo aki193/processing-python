730c4b7d33bc5d082a436f5387336001815c2431
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2018-01-04 21:53:27 +0900

    [wip] Added animation function

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 def predict(wvec,xvec):
     out=np.dot(wvec,xvec)
@@ -19,11 +20,24 @@ def train(wvec,xvec,label):
     else:
         return wvec
 
+def plot(i, wvec):
+    for w in wvec:
+        plt.cla()
+        x_fig=range(-15,16)
+        y_fig=[-(w[1]/w[0])*xi-(w[2]/w[1]) for xi in x_fig]
+
+        plt.scatter(x1[:,0],x1[:,1],marker='o',color='g',s=100)
+        plt.scatter(x2[:,0],x2[:,1],marker='s',color='b',s=100)
+        im = plt.plot(x_fig,y_fig)
+
 if __name__=='__main__':
 
     item_num=100
     loop=1000
     init_wvec=[1,-1,1]
+    isUpdate = True     # 収束判定
+    ims = []
+
 
     x1_1=np.ones(int(item_num/2))+10*np.random.random(int(item_num/2))
     x1_2=np.ones(int(item_num/2))+10*np.random.random(int(item_num/2))
@@ -40,9 +54,6 @@ if __name__=='__main__':
 
     wvec=np.vstack((init_wvec,init_wvec))
 
-    isUpdate = True
-
-    # for j in range(5):
     # 重みベクトルが更新されている場合はループする
     while isUpdate:
         # 更新がない状態にする
@@ -53,15 +64,30 @@ if __name__=='__main__':
             if not np.allclose(wvec[-1], wvec_new):
                 isUpdate = True
             wvec=np.vstack((wvec,wvec_new))
-        print isUpdate
-    w=wvec[-1]
-    print w
 
-    x_fig=range(-15,16)
-    y_fig=[-(w[1]/w[0])*xi-(w[2]/w[1]) for xi in x_fig]
-    print y_fig
+    # w=wvec[-1]
+    # print w
 
-    plt.scatter(x1[:,0],x1[:,1],marker='o',color='g',s=100)
-    plt.scatter(x2[:,0],x2[:,1],marker='s',color='b',s=100)
-    plt.plot(x_fig,y_fig)
+    fig = plt.figure()
+
+    # for w in wvec:
+    #     x_fig=range(-15,16)
+    #     y_fig=[-(w[1]/w[0])*xi-(w[2]/w[1]) for xi in x_fig]
+    #
+    #     plt.scatter(x1[:,0],x1[:,1],marker='o',color='g',s=100)
+    #     plt.scatter(x2[:,0],x2[:,1],marker='s',color='b',s=100)
+    #     im = plt.plot(x_fig,y_fig)
+    #     ims.append(im)
+
+    # ani = animation.ArtistAnimation(fig, ims, interval=100)
+    ani = animation.FuncAnimation(fig, plot, fargs=wvec, interval=100, frames=10)
     plt.show()
+
+    # ims.append(im)
+    # x_fig=range(-15,16)
+    # y_fig=[-(w[1]/w[0])*xi-(w[2]/w[1]) for xi in x_fig]
+    #
+    # plt.scatter(x1[:,0],x1[:,1],marker='o',color='g',s=100)
+    # plt.scatter(x2[:,0],x2[:,1],marker='s',color='b',s=100)
+    # plt.plot(x_fig,y_fig)
+    # plt.show()
